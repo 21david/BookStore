3bc1f1d62ed09020f5d1f354209e3075fbea3839
David Espinosa <david.espinosa.21@gmail.com>
2025-07-08 15:35:05 -0500

    Add Angular and Bootstrap front-end

---
+++ bookstore-front/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'bs-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styles: [],
+})
+export class App {
+  protected title = 'Nile BookStore';
+}
